aa007ff059c140ed072bd98bad1655aa9fba9221
Arkhipkina <106983092+Arkhipkina@users.noreply.github.com>
2024-08-08 20:16:34 +0300

    add links to cards

--- main.py
+++ main.py
@@ -1,24 +1,33 @@
 import requests
+import json
 from bs4 import BeautifulSoup
 
 def get_cars_cards(response):
     soup = BeautifulSoup(response.text, 'lxml')
+    cards_tag = soup.find(class_="css-1nvf6xk ejck0o60").find_all(class_='css-1f68fiz ea1vuk60')
+
+
+    for card in cards_tag:
+        link_tag = card.find(class_="css-10vnevh e1pqv6mt0").find_all("a")
+        for link in link_tag:
+            car_link = link.get("href")
+
+
 
 
 
 def main():
-    cars = ['mercedes-benz', 'tesla', 'ford', 'bmw', 'kia']
+    #cars = ['mercedes-benz', 'tesla', 'ford', 'bmw', 'kia']
 
-    for car in cars:
-        url = f'https://spb.drom.ru/{car}/all/'
+    #for car in cars:
+        url = f'https://spb.drom.ru/mercedes-benz/all/'
         params = {"ph": 1, 
                   "unsold": 1}
         response = requests.get(url, params=params)
         response.raise_for_status()
-        print(response.text)
 
-        get_cars_cards(response)
-
+        cards = get_cars_cards(response)
+        # download_images(response, cards)
 
 if __name__ == "__main__":
     main()
